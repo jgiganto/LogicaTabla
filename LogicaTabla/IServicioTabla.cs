2ee010ef4df30b9d093b1e1b197a51e3ca27ac91
jgiganto <jgiganto1@hotmail.com>
2018-03-14 14:15:11 +0100

    d

--- LogicaTabla/IServicioTabla.cs
+++ LogicaTabla/IServicioTabla.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace LogicaTabla
 {
-    public class Class1
+    [ServiceContract]
+    interface IServicioTabla
     {
+        [OperationContract]
+        List<int> GetSecuenciaTabla(int numero);
     }
 }
---
+++ LogicaTabla/ServicioTabla.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaTabla
+{
+    public class ServicioTabla : IServicioTabla
+    {
+        public List<int> GetSecuenciaTabla(int numero)
+        {
+            List<int> resultado = new List<int>();
+            
+            for(int i = 0; i < 10; i++)
+            {
+                int producto = i * numero;
+                resultado.Add(producto);
+            }
+            return resultado;
+        }
+    }
+}
